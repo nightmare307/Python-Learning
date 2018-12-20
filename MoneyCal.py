3808a18beace91d4e67f315d4b9cd75bf55aebd8
nightmare307 <nightmare307@hotmail.com>
2018-12-20 23:33:56 +0800

    还是没写好

--- MoneyCal.py
+++ MoneyCal.py
@@ -10,10 +10,10 @@ print('--------'*6+'还款计算器'+'--------'*7)
 
 #定义还款方式
 mode=input('请输入还款类型(等额本金/等额本息)：')
-while mode == '' :
+mode1=['等额本金','等额本息']
+while mode not in mode1  :
     print('请输入正确的还款类型(等额本金/等额本息)：')
     mode = input('请输入还款类型(等额本金/等额本息)：')
-
 #定义初始期数变量
 i = 1
 #定义期数变量
@@ -30,8 +30,8 @@ while capital <=0:
 #定义利率
 #dayrate= 0.0004
 dayrate=float(input('请输入借款日利率(百分比)：'))*0.01
-monthrate = float(input('请输入借款月利率(百分比)：'))*0.01
-yearrate = float(input('请输入借款年利率(百分比)：'))*0.01
+#monthrate = float(input('请输入借款月利率(百分比)：'))*0.01
+#yearrate = float(input('请输入借款年利率(百分比)：'))*0.01
 #定义剩余本金
 recapital = 0
 #定义每月应还本金
@@ -110,7 +110,7 @@ if mode is '等额本金':
        i+=1
 
 #-----------------------------------------等额本息（未完成）-----------------------------------
-'''
+
 elif mode is '等额本息':
     print('还没做')
     
@@ -120,15 +120,15 @@ elif mode is '等额本息':
     while i <= terms:
        if i == 1:
            recapital = capital #定义剩余本金
-           if dayuse = (datefirstback-datefirst).days>30:
+           dayuse = (datefirstback-datefirst).days
+           if dayuse-30>0:
                dayuse1=dayuse-30
            monthall=monthall+dayuse1*dayrate
-           interest=capital*dayrate*30*i
+           interest=capital*dayrate*30
            monthcapital=mothall-interest
            print('第%d期，剩余待还本金%d，还款本金%.2f元，当期利息%.2f元，还款本息合计%.2f元，还款日为%s，使用天数%d天' % (
                i, recapital, monthcapital, interest, monthall, datefirstback, dayuse))
 
-'''
 #本息求和
 totalsum = sum(totalall)
 #利息求和
