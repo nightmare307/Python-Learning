e666010c69782e4299fbb3bf36bdac71cc01aadd
nightmare307 <nightmare307@hotmail.com>
2018-12-20 23:33:56 +0800

    还是没写好,恢复回去

--- MoneyCal.py
+++ MoneyCal.py
@@ -9,11 +9,11 @@ from dateutil.relativedelta import relativedelta
 print('--------'*6+'还款计算器'+'--------'*7)
 
 #定义还款方式
-mode=input('请输入还款类型(等额本金/等额本息)：')
-while mode == '' :
-    print('请输入正确的还款类型(等额本金/等额本息)：')
-    mode = input('请输入还款类型(等额本金/等额本息)：')
-
+mode=input('请输入还款类型(等额本息)：')
+mode1=['等额本息']
+while mode not in mode1  :
+    print('请输入正确的还款类型(等额本息)：')
+    mode = input('请输入还款类型(等额本息)：')
 #定义初始期数变量
 i = 1
 #定义期数变量
@@ -30,8 +30,8 @@ while capital <=0:
 #定义利率
 #dayrate= 0.0004
 dayrate=float(input('请输入借款日利率(百分比)：'))*0.01
-monthrate = float(input('请输入借款月利率(百分比)：'))*0.01
-yearrate = float(input('请输入借款年利率(百分比)：'))*0.01
+#monthrate = float(input('请输入借款月利率(百分比)：'))*0.01
+#yearrate = float(input('请输入借款年利率(百分比)：'))*0.01
 #定义剩余本金
 recapital = 0
 #定义每月应还本金
@@ -48,11 +48,11 @@ else:
     datefirst = datetime.datetime(fy, fm, fd).date()
 #定义第一期还款日
 #datefirstback = datetime.date(2018, 12, 24)
-fbacka = input('请输入第一期还款日（yyyy-mm-dd）：')
+fbacka = input('请输入首期还款日（yyyy-mm-dd）：')
 #当输入值为空时提示并重新输入
 while fbacka=='':
    print('首期还款日不可为空，请重新输入！')
-   fbacka = input('请输入第一期还款日（yyyy-mm-dd）：')
+   fbacka = input('请输入首期还款日（yyyy-mm-dd）：')
 #计算初次还款日
 fbackt = time.strptime(fbacka, "%Y-%m-%d")
 fby, fbm, fbd = fbackt[0:3]
@@ -60,7 +60,7 @@ datefirstback = datetime.datetime(fby, fbm, fbd).date()
 #比较首期还款日是否小于借款日并提示重新输入
 while datefirstback < datefirst:
    print('首期还款日不可早于借款日期，请重新输入！')
-   fbacka = input('请输入第一期还款日（yyyy-mm-dd）：')
+   fbacka = input('请输入首期还款日（yyyy-mm-dd）：')
    fbackt = time.strptime(fbacka, "%Y-%m-%d")
    fby, fbm, fbd = fbackt[0:3]
    datefirstback = datetime.datetime(fby, fbm, fbd).date()
@@ -109,31 +109,11 @@ if mode is '等额本金':
 
        i+=1
 
-#-----------------------------------------等额本息（未完成）-----------------------------------
-'''
-elif mode is '等额本息':
-    print('还没做')
-    
-    print('--------'*7+'还款计划'+'--------'*7)
-    totalac=capital+interest
-    monthall=total/totalac
-    while i <= terms:
-       if i == 1:
-           recapital = capital #定义剩余本金
-           if dayuse = (datefirstback-datefirst).days>30:
-               dayuse1=dayuse-30
-           monthall=monthall+dayuse1*dayrate
-           interest=capital*dayrate*30*i
-           monthcapital=mothall-interest
-           print('第%d期，剩余待还本金%d，还款本金%.2f元，当期利息%.2f元，还款本息合计%.2f元，还款日为%s，使用天数%d天' % (
-               i, recapital, monthcapital, interest, monthall, datefirstback, dayuse))
-
-'''
 #本息求和
 totalsum = sum(totalall)
 #利息求和
 totalint = sum(interestall)
-print('--------'*7+'合    计'+'--------'*7)
+print('-------'*7+'合    计'+'-------'*7)
 print('共%d期，日利率%.2f%%，月利率%.3f%%，年利率（365天计算）%.3f%%,年利率（360天计算）%.3f%%'%
       (terms, dayrate*100, dayrate*30*100, dayrate*365*100,dayrate*360*100))
 print('总计还款%.2f元，其中本金总计%.2f元，利息合计%.2f元'%(totalsum,capital,totalint))
