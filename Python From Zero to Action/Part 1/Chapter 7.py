841484945fafa95799e92b47229afc0e980f54a7
nightmare307 <nightmare307@hotmail.com>
2019-01-08 17:19:38 +0800

    第七章学习

--- Python From Zero to Action/Part 1/Chapter 7.py
+++ Python From Zero to Action/Part 1/Chapter 7.py
@@ -24,3 +24,44 @@ print('立方体体积是%d' % (myBox2.volume()))
 
 bl=myBox.length
 print(bl)
+#==========================类的继承==================================
+class BoxClassN():
+    def __init__(self, Boxlength, Boxwidth, Boxheight):
+        self.length = Boxlength
+        self.width = Boxwidth
+        self.height = Boxheight
+    def volume(self):
+        return self.length*self.width*self.height
+#=========================以下是继承类===============================
+class BoxClassIn(BoxClassN):
+    def __init__(self, Boxlength, Boxwidth, Boxheight):
+        super().__init__(Boxlength, Boxwidth, Boxheight)
+        self.color='white'
+        self.material='paper'
+        self.type='fish'
+    def area(self):
+        superficial=self.length*self.height+self.width*self.height+self.length*self.width
+        return superficial*2
+#主程序调用
+myBox3=BoxClassIn(10,8,3)
+print('立方体体积是%d，表面积是%d，颜色是%s，材质是%s，类型是%s'%(myBox3.volume(),myBox3.area(),myBox3.color,myBox3.material,myBox3.type))
+
+#==========================================重写方法=========================================
+class BoxClassRe(BoxClassN):
+    def __init__(self, Boxlength, Boxwidth, Boxheight):
+        super().__init__(Boxlength, Boxwidth, Boxheight)
+    def volume(self,num=1):
+        return self.length*self.width*self.height*num
+myBox2 = BoxClassRe(8, 8, 2)
+print('10个立方体体积是%d' % (myBox2.volume(10)))
+#=========================================私有化===========================================
+class TeatPrivate():
+    def __init__(self):
+        self.__say='ok'
+    def p(self):
+        print(self.__say)
+    def __p1(self):
+        print(self.__say)
+#======================私有类调用=======================
+show=TeatPrivate()
+show.p()
