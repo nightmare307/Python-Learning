bd596ab7605db4c38bd12af9e722dd457f57bfa3
nightmare307 <nightmare307@hotmail.com>
2019-01-08 23:22:26 +0800

    第二章 字符串练习

--- Python Crash Course/Part 1/Chapter 2.py
+++ Python Crash Course/Part 1/Chapter 2.py
@@ -4,4 +4,11 @@ print ("hello world")
 #2.2多条简单消息
 print("hello world")
 print("hello Python world")
-
+#2.3个性化消息 2.4调整名字大小写
+name='    Chris   '
+print('Hello '+name+' , would you like to learn some Python today?')
+print('Hello '+name.upper()+' , would you like to learn some Python today?')
+print('Hello '+name.lower()+' , would you like to learn some Python today?')
+print('Hello  '+name.lower().strip()+' , would you like to learn some Python today?')
+print('Hello '+name.lower().lstrip()+' , would you like to learn some Python today?')
+print('Hello '+name.lower().rstrip()+' , would you like to learn some Python today?')
