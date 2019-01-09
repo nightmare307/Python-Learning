d8d489f3e7928ee198128965b3c26eacd7495376
nightmare307 <nightmare307@hotmail.com>
2019-01-09 18:34:45 +0800

    第二章学习

--- Python Crash Course/Part 1/Chapter 2.py
+++ Python Crash Course/Part 1/Chapter 2.py
@@ -12,3 +12,22 @@ print('Hello '+name.lower()+' , would you like to learn some Python today?')
 print('Hello  '+name.lower().strip()+' , would you like to learn some Python today?')
 print('Hello '+name.lower().lstrip()+' , would you like to learn some Python today?')
 print('Hello '+name.lower().rstrip()+' , would you like to learn some Python today?')
+#2.5名言
+print('Albert Einstein once said, "A person who never made a mistake never tried anything new."')
+#2.6名言2
+famous_person='Chris Lee'
+message = 'Albert Einstein once said, “A person who never made a mistake never tried anything new.”'
+print(famous_person+' once said '+message)
+#2.8 数字8
+print(2*4)
+print(16-8)
+print(8.0/1)
+print(4+4)
+#2.9最喜欢的数字
+favourite=4
+print('My favourite number is '+str(favourite))
+#2.10添加注释
+#2.11Zen of Python
+import this
+zen = "".join([this.d.get(c, c) for c in this.s])
+print(zen)
