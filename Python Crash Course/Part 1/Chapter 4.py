08bbbe6e3b2a515798bfbcc39b82dcc441a0b004
nightmare307 <nightmare307@hotmail.com>
2019-01-11 17:08:57 +0800

    第四章练习完成，开始第五章练习

--- Python Crash Course/Part 1/Chapter 4.py
+++ Python Crash Course/Part 1/Chapter 4.py
@@ -38,3 +38,26 @@ print(Cubes)
 #4.9立方解析
 cubes=[cube**3 for cube in range(1,11)]
 print(cubes)
+
+
+#4.10切片
+brand=['lenovo','apple','acer','hp','thinkpad','nec','sony','dell']
+print('The first three items in the list are' ,brand[0:3])
+print('Three items from the middle of the list are',brand[2:5])
+
+#4.11
+phones=['apple','nokia','samsung','lenovo']
+nphones=phones[:]
+phones.insert(4,'xiaomi')
+nphones.append('meizu')
+print('My favourite phones are',[phone for phone in phones])
+#4.12 使用多个循环
+print("My friend's favourite phones are", [nphone for nphone in nphones])
+
+#4.13自助餐
+buffet = ('Humburg', 'Pizza', 'Cheese', 'Steak','Tomato')
+for dish in buffet:
+    print(dish)
+buffet= ('Humburg', 'Pizza', 'Cheese', 'Steak', 'Tomato','Sushi','Pancake')
+for dishes in buffet:
+    print(dishes)
