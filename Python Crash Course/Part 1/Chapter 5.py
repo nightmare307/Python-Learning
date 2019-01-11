23a2be93d3c6902c6b763f6a371e098170933751
nightmare307 <nightmare307@hotmail.com>
2019-01-11 19:08:06 +0800

    第五章练习做到5-7

--- Python Crash Course/Part 1/Chapter 5.py
+++ Python Crash Course/Part 1/Chapter 5.py
@@ -1,1 +1,41 @@
 # -*- coding: utf-8 -*
+#5.1条件测试
+car = 'subaru' 
+print("Is car == 'subaru'? I predict True.") 
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.") 
+print(car == 'audi')
+
+
+#5.3外星人颜色#1
+alien='red'
+if alien=='green':
+    print('You get 5 points')
+if alien != 'green':
+    print('You get 1 point')
+
+#5.4外星人颜色#2
+aliens='green'
+if aliens=='green':
+    print('You get 5 points')
+else:
+    print('You get 1 point')
+
+aliens='red'
+if aliens == 'green':
+    print('You get 5 points')
+else:
+    print('You get 1 point')
+
+#5.5外星人颜色#3
+alienlist=['green','yellow','red']
+for alienc in alienlist:
+    if alienc == 'green':
+        points=5
+    elif alienc == 'yellow':
+        points=10
+    elif alienc=='red':
+        points=15
+    print('You get %d points'%points)
+
+#5.6人生的不同阶段
